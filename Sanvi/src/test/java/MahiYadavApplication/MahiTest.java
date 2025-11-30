abc0cfa69365cbed431adf954cdb7240e0b08167
rogers <rogers@localhost>
2025-11-30 21:50:40 +0530

    wd

--- Sanvi/src/test/java/MahiYadavApplication/MahiTest.java
+++ Sanvi/src/test/java/MahiYadavApplication/MahiTest.java
@@ -6,6 +6,37 @@ public class MahiTest {
 		
 		System.out.println("Hello");
 		System.out.println("Hello");
+		int a = 1;
+		
+		while(a<=23)
+		{
+			System.out.println(a);
+			a++;
+		}
+		
+		System.out.println();  //to change the line 
+		
+		// to print even numbers 
+		
+		int b = 2;
+		
+		while(b<=23)
+		{
+			System.out.println(b);
+			b+=2;
+		}
+		
+		System.out.println(); //to change the line 
+		
+		//to print decrement numbers
+		
+		int i = 20;
+		
+		while(i>0)
+		{
+			System.out.println(i);
+			i--;
+		}
 
 	}
 
